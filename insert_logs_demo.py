f93333273bdbc93b0ed84573fac679effea08c15
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-07-04 17:04:54 +0530

    few changes

--- insert_logs_demo.py
+++ insert_logs_demo.py
@@ -1,16 +1,12 @@
-# from elasticsearch import Elasticsearch
-# from datetime import datetime
 
-# # Connect to Elasticsearch
-# es = Elasticsearch("http://localhost:9200")
+from elasticsearch import Elasticsearch
+from datetime import datetime
 
-# # Sample log entry
-# log = {
-#     "timestamp": datetime.now().isoformat(),
-#     "level": "ERROR",
-#     "message": "Unauthorized login attempt detected."
-# }
-
-# # Insert log into 'logs' index
-# res = es.index(index="logs", document=log)
-# print("✅ Log inserted:", res['result'])
+es = Elasticsearch("http://localhost:9200")
+log = {
+    "timestamp": datetime.now().isoformat(),  # Current timestamp
+    "level": "ERROR",
+    "message": "🚨 Manual test: ERROR breach simulation"
+}
+res = es.index(index="logs", document=log)
+print("✅ Log inserted:", res['result'])
